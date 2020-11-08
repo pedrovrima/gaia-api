4d4f4223b9e2d108c252b2d3b4c150d721247418
Pedro Martins <pedrovrima@gmail.com>
2020-11-08 15:48:23 -0300

    puts squelize back

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require("express");
-// const { Models } = require('./app/models');
 var bodyParser = require("body-parser");
-// var species_controller = require('./controllers/species')
-// var autu_controller = require('./controllers/autu')
+var species_controller = require('./controllers/species')
+var autu_controller = require('./controllers/autu')
 
 // configure the app to use bodyParser()
 console.log("start");
@@ -23,17 +22,17 @@ app.get("/favicon.ico", function (req, res) {
   res.sendStatus(204);
 });
 
-// app.post('/create_spp', ( req,res) => {
-//   console.log("start")
+app.post('/create_spp', ( req,res) => {
+  console.log("start")
 
-//   species_controller.create_species(req,res);
-// });
+  species_controller.create_species(req,res);
+});
 
-// app.post('/create_autu', ( req,res) => {
-// autu_controller.create_autu(req,res)});
+app.post('/create_autu', ( req,res) => {
+autu_controller.create_autu(req,res)});
 
-// app.get('/get_species',(req,res)=>{
-//   species_controller.get_species(req,res)
-// })
+app.get('/get_species',(req,res)=>{
+  species_controller.get_species(req,res)
+})
 
 app.listen(process.env.PORT || 5000);
