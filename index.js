9290281c1b511d7058fb267f27e2ddd3a1487f89
Pedro Martins <pedrovrima@gmail.com>
2020-11-08 15:27:42 -0300

    teste

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 // const { Models } = require('./app/models');
 var bodyParser = require('body-parser');
-var species_controller = require('./controllers/species')
-var autu_controller = require('./controllers/autu')
+// var species_controller = require('./controllers/species')
+// var autu_controller = require('./controllers/autu')
 
 // configure the app to use bodyParser()
 console.log("start")
@@ -18,22 +18,22 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));})
 
 
-app.post('/create_spp', ( req,res) => {
-  console.log("start")
+// app.post('/create_spp', ( req,res) => {
+//   console.log("start")
 
-  species_controller.create_species(req,res);
-});
-
-
-app.post('/create_autu', ( req,res) => {
-autu_controller.create_autu(req,res)});
+//   species_controller.create_species(req,res);
+// });
 
 
+// app.post('/create_autu', ( req,res) => {
+// autu_controller.create_autu(req,res)});
 
 
-app.get('/get_species',(req,res)=>{
-  species_controller.get_species(req,res)
-})
+
+
+// app.get('/get_species',(req,res)=>{
+//   species_controller.get_species(req,res)
+// })
 
 
 app.listen(3000);
