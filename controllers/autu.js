9e873336367a216c6a6b35eb9ad33eefb925d406
Pedro Martins <pedrovrima@gmail.com>
2020-11-08 21:24:12 -0300

    changes

--- controllers/autu.js
+++ controllers/autu.js
@@ -1,8 +1,8 @@
 const db = require("../app/models");
 
 const create_autu = async (req, res) => {
-  const { autu_info } = req.body;
-  try{console.log(req.body)
+  const  autu_info = req.body;
+  try{
  await db.Autu.create(autu_info, { include: [
       {model:db.Infractions,as:"infractions"},
       {model:db.Species_Autu,as:"species"},
