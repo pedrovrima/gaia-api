1cbcd72c5abd1122729297bdd9b6642922c93b53
Pedro Martins <pedrovrima@gmail.com>
2020-11-08 21:17:27 -0300

    checks

--- controllers/autu.js
+++ controllers/autu.js
@@ -2,11 +2,16 @@ const db = require("../app/models");
 
 const create_autu = async (req, res) => {
   const { autu_info } = req.body;
-  db.Autu.create(autu_info, { include: [
+  try{console.log(req.body)
+ await db.Autu.create(autu_info, { include: [
       {model:db.Infractions,as:"infractions"},
       {model:db.Species_Autu,as:"species"},
       {model:db.Individuals_Autu,as:"individuals"}
-  ] });
+  ] })
+  res.sendStatus(200)
+  ;}catch{e=>console.log(e)
+
+  }
 };
 
 module.exports = { create_autu };
